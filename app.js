7406a2786feb19ed766e047ff81e46065d070ca5
kws7327 <kws7327@naver.com>
2021-11-16 01:44:45 +0900

    Comment
    1

--- app.js
+++ app.js
@@ -174,3 +174,214 @@ app.post("/Signout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
 });
+
+//Use 쓰면 되냐 걍?
+app.use("/ImageLinking", function (req, res) {
+  console.log("ImageLinking");
+
+  const path = req.query.Path;
+  const filename = req.query.Filename;
+  console.log(path, filename);
+
+  res.sendFile(__dirname + path + filename);
+});
+//path.join(__dirname, "/assets/Catlas_Gallery/2021", "Corner1.png")
+
+router.route("/Board").get(function (req, res) {
+  const menu = req.query.BoardPath;
+
+  console.log("Get " + menu + " Board");
+
+  if (!isNaN(menu) || menu === "down") {
+    // Gallery 일때
+    console.log("Gallery");
+    const sql =
+      "SELECT * FROM gallery WHERE menu='" + menu + "' order by idx asc";
+
+    con.query(sql, (err, results) => {
+      const Tojson = JSON.parse(JSON.stringify(results));
+      if (err) throw err;
+      return res.send(Tojson);
+    });
+  } else {
+    const sql =
+      "SELECT menu,writer,date,views,idx,title FROM board WHERE menu='" +
+      menu +
+      "' order by idx asc";
+
+    if (isNaN(menu)) {
+      con.query(sql, (err, results) => {
+        const Tojson = JSON.parse(JSON.stringify(results));
+
+        Tojson.contents = undefined;
+        if (err) throw err;
+        return res.send(Tojson);
+      });
+    }
+  }
+});
+
+router.route("/Detail").get(function (req, res) {
+  const idx = req.query.PostNum;
+  const menu = req.query.BoardPath;
+
+  console.log("Get " + menu + "의 " + idx + "번글 호출");
+
+  // 일반 게시판일때
+  const sql =
+    "SELECT * FROM board WHERE menu='" +
+    menu +
+    "' AND idx=" +
+    idx +
+    " order by idx asc";
+
+  const ViewsPlussql =
+    "UPDATE board SET views = views + 1 WHERE menu='" +
+    menu +
+    "' AND idx=" +
+    idx +
+    ";";
+
+  con.query(ViewsPlussql, (err, results) => {});
+
+  // 댓글 => Forum 일때만
+
+  if (
+    menu === "자유게시판" ||
+    menu === "질문게시판" ||
+    menu === "홍보게시판" ||
+    menu === "동아리게시판" ||
+    menu === "IT게시판" ||
+    menu === "자료실"
+  ) {
+    const cmtSql =
+      "SELECT * FROM comment WHERE menu='" +
+      menu +
+      "' AND target=" +
+      idx +
+      " order by idx asc";
+    con.query(cmtSql, (err, results) => {
+      const Tojson = JSON.parse(JSON.stringify(results));
+    });
+  }
+
+  if (!isNaN(idx)) {
+    // 숫자이여야지만 들어가게
+    con.query(sql, (err, results) => {
+      const Tojson = JSON.parse(JSON.stringify(results));
+      if (err) throw err;
+      return res.send(Tojson);
+    });
+  }
+});
+
+router.route("/Home").get(function (req, res) {
+  console.log("Get Home");
+
+  let ResJson = [];
+  // 일반 게시판일때
+  const Noticesql =
+    "SELECT title,contents,idx,date FROM board WHERE menu='공지사항' order by idx desc LIMIT 2";
+
+  const FreeForumsql =
+    "SELECT title,contents,idx,date FROM board WHERE menu='자유게시판' order by idx desc LIMIT 1";
+
+  const QuestionForumsql =
+    "SELECT title,contents,idx,date FROM board WHERE menu='질문게시판' order by idx desc LIMIT 1";
+
+  const AdvertisingForumsql =
+    "SELECT title,contents,idx,date FROM board WHERE menu='홍보게시판' order by idx desc LIMIT 1";
+
+  con.query(Noticesql, (err, results) => {
+    const Tojson = JSON.parse(JSON.stringify(results));
+    if (err) throw err;
+    ResJson.push(Tojson);
+  });
+
+  con.query(FreeForumsql, (err, results) => {
+    const Tojson = JSON.parse(JSON.stringify(results));
+    if (err) throw err;
+    ResJson.push(Tojson);
+  });
+
+  con.query(QuestionForumsql, (err, results) => {
+    const Tojson = JSON.parse(JSON.stringify(results));
+    if (err) throw err;
+    ResJson.push(Tojson);
+  });
+
+  con.query(AdvertisingForumsql, (err, results) => {
+    const Tojson = JSON.parse(JSON.stringify(results));
+    if (err) throw err;
+    ResJson.push(Tojson);
+    return res.send(ResJson);
+  });
+});
+
+router.route("/Test").get(function (req, res) {
+  // 크롤링을 위한 import (axios, cheerio)
+  const axios = require("axios");
+  const cheerio = require("cheerio");
+
+  let currentPage = 1;
+  let url_notice =
+    "https://newgh.gnu.ac.kr/cs/na/ntt/selectNttList.do?mi=6694&bbsId=2351&currPage=";
+
+  let url_noticeSW =
+    "https://newgh.gnu.ac.kr/cs/na/ntt/selectNttList.do?mi=6695&bbsId=2352&currPage=";
+
+  // 게시판 - 공지사항
+  const getCurrURL_notice = (url) => {
+    url = url + currentPage.toString();
+    return url;
+  };
+
+  // 게시판 - 공지사항(SW)
+  const getCurrURL_noticeSW = (url) => {
+    url =
+      "https://newgh.gnu.ac.kr/cs/na/ntt/selectNttList.do?mi=6695&bbsId=2352&currPage=" +
+      currentPage.toString();
+    return url;
+  };
+
+  //axios를 이용한 통신
+  const getHTML = async (url) => {
+    try {
+      return await axios.get(getCurrURL_notice(url));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  getHTML(url_notice)
+    .then((html) => {
+      let postList = [];
+      const $ = cheerio.load(html.data);
+      const $bodyList = $("div.BD_list table tbody").children("tr");
+      $bodyList.each(function (i, elem) {
+        postList[i] = {
+          title: $(this).find("td.ta_l a").text().trim(),
+          writer: $(this).find("td:nth-of-type(3)").text().trim(),
+          date: $(this).find("td:nth-of-type(4)").text().trim(),
+        };
+      });
+      return postList;
+    })
+    .then((res) => console.log("결과", res));
+
+  getHTML(url_noticeSW)
+    .then((html) => {
+      let postList = [];
+      const $ = cheerio.load(html.data);
+      const $bodyList = $("div.BD_list table tbody").children("tr");
+      $bodyList.each(function (i, elem) {
+        postList[i] = {
+          title: "[SW] " + $(this).find("td.ta_l a").text().trim(),
+          writer: $(this).find("td:nth-of-type(3)").text().trim(),
+          date: $(this).find("td:nth-of-type(4)").text().trim(),
+        };
+      });
+      return postList;
+    })
+    .then((res) => console.log("결과", res));
+});
